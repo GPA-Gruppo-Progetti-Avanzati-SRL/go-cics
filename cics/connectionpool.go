1224142aab612a574180fc5f356ae9525b5719b8
simone.tiberti <simone.tiberti@gpagroup.it>
2024-07-10 17:55:28 +0200

    metrics

--- cics/connectionpool.go
+++ cics/connectionpool.go
@@ -11,6 +11,7 @@ import "C"
 import (
 	"context"
 	pool "github.com/jolestar/go-commons-pool/v2"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/rs/zerolog/log"
 	"time"
 	"unsafe"
@@ -19,9 +20,40 @@ import (
 var p *pool.ObjectPool
 var ctx = context.Background()
 
+type Metrics struct {
+	GetConnection      prometheus.Counter
+	ReturnedConnection prometheus.Counter
+	ActiveConnection   prometheus.Gauge
+}
+
+var metrics *Metrics
+
+func NewMetrics(reg prometheus.Registerer) *Metrics {
+	m := &Metrics{
+		GetConnection: prometheus.NewCounter(prometheus.CounterOpts{
+			Name: "get_connection_count",
+			Help: "Number of get connections requested.",
+		}),
+		ReturnedConnection: prometheus.NewCounter(prometheus.CounterOpts{
+			Name: "returned_connection_count",
+			Help: "Number of get connections returned to the pool.",
+		}),
+		ActiveConnection: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "active_connection_count",
+			Help: "Number of active connections.",
+		}),
+	}
+	reg.MustRegister(m.GetConnection)
+	reg.MustRegister(m.ReturnedConnection)
+	reg.MustRegister(m.ActiveConnection)
+	return m
+}
+
 func InitConnectionPool(config *ConnectionConfig) {
 	tokenChannel := make(chan *C.CTG_ConnToken_t, 5)
 	eciChannel := make(chan *C.ECI_ChannelToken_t)
+	reg := prometheus.NewRegistry()
+	metrics = NewMetrics(reg)
 	p = pool.NewObjectPool(ctx, &ConnectionFactory{Config: config, TokenChannel: tokenChannel, EciChannel: eciChannel}, &pool.ObjectPoolConfig{
 		LIFO:                     true,
 		MaxTotal:                 config.MaxTotal,
@@ -48,6 +80,8 @@ func InitConnectionPool(config *ConnectionConfig) {
 		<-proxyready
 		log.Info().Msg("Socket opened")
 	}
+
+	// Create new metrics and register them using the custom registry.
 
 	go ListeningClosure(tokenChannel)
 	go ChannelClosure(eciChannel)
@@ -89,6 +123,8 @@ func CloseConnectionPool() {
 
 func GetConnection() (*Connection, error) {
 	cicsConnection, err := p.BorrowObject(ctx)
+	metrics.GetConnection.Inc()
+	metrics.ActiveConnection.Inc()
 	if err != nil {
 		log.Error().Msgf("Error getting connection from pool: %v", err)
 		return nil, err
@@ -104,5 +140,7 @@ func GetConnection() (*Connection, error) {
 }
 
 func ReturnConnection(cicsConnection *Connection) error {
+	metrics.ReturnedConnection.Inc()
+	metrics.ActiveConnection.Dec()
 	return p.ReturnObject(ctx, cicsConnection)
 }
--- go.mod
+++ go.mod
@@ -11,6 +11,8 @@ require (
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.4 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -19,8 +21,13 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.19.1 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.48.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
 	golang.org/x/crypto v0.25.0 // indirect
 	golang.org/x/net v0.27.0 // indirect
 	golang.org/x/sys v0.22.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
